f0bbae7fb1d985ff031defd1c2e7e0dd3d1ea59e
funny_is_me <duy.dong4125@hcmut.edu.vn>
2025-09-02 22:31:04 +0700

    Update about.cpp
    //Thay doi commit tren web github

--- about.cpp
+++ about.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main(){
     cout<<"Dai hoc Bach Khoa TPHCM !"<<endl;
     cout<<"KTMT"<<endl;
+    //Thay doi commit tren web 
+    cout<<"Toi iu VLSI"<<endl;
     return 0;
 }
