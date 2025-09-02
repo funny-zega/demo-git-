64ebc2fcb93309876065d3e0bd672e7697586830
funny-zega <duy.dong4125@hcmut.edu.vn>
2025-09-02 23:09:13 +0700

    Add them tu branch

--- huong-dan-add-branch.cpp
+++ huong-dan-add-branch.cpp
@@ -6,7 +6,23 @@ int main(){
     cout<<"Huong dan add branch"    <<endl;
     cout<<"Them dong 2"<<endl;
     cout<<"Them dong 3"<<endl;
-      
+        cout<<"Them dong 4"<<endl;
+        cout<<"Them dong 5"<<endl;
+        cout<<"Them dong 6"<<endl;
+        cout<<"Them dong 7"<<endl;
+        cout<<"Them dong 8"<<endl;
+        cout<<"Them dong 9"<<endl;
+        cout<<"Them dong 10"<<endl;
+        cout<<"Them dong 11"<<endl;
+        cout<<"Them dong 12"<<endl;
+        cout<<"Them dong 13"<<endl;
+        cout<<"Them dong 14"<<endl;
+        cout<<"Them dong 15"<<endl;
+        cout<<"Them dong 16"<<endl;
+        cout<<"Them dong 17"<<endl;
+        cout<<"Them dong 18"<<endl;
+        cout<<"Them dong 19"<<endl;
+        cout<<"Them dong 20"<<endl;
     return 0;
 
 }
